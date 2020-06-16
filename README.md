64304c6881c3f2953899bc082efe862dd1355b52
Bruno Martins <brunorodrigues929@gmail.com>
2020-06-16 02:46:33 -0300

    Projects CRUD, Likes

---
+++ README.md
@@ -0,0 +1,44 @@
+![Bootcamp](https://camo.githubusercontent.com/d25397e9df01fe7882dcc1cbc96bdf052ffd7d0c/68747470733a2f2f73746f726167652e676f6f676c65617069732e636f6d2f676f6c64656e2d77696e642f626f6f7463616d702d676f737461636b2f6865616465722d6465736166696f732e706e67)
+
+# Esta é a resolução do [DESAFIO 02](https://github.com/Rocketseat/bootcamp-gostack-desafios/blob/master/desafio-conceitos-nodejs/README.md) do Bootcamp GoStack 2020.
+&nbsp;
+##### Este projeto apresenta o uso de conceitos iniciais/ introdutórios ao Node.js -v12.18.0 e inclui:
+#### Metodos HTTP
+- GET
+- POST 
+- PUT
+- DELETE
+#### Parametros do Tipo
+- Query Params
+- Route Params
+- Body Params
+
+Inclui também o uso de _**middleware**_ para valiação antes da rota desejada. 
+&nbsp;
+# Este projeto foi criado/testado com:
+    -yarn -v1.x.x
+    -Nodejs -v12.x.x
+ _O não uso desses requisitos pode atrapalhar a execussão do projeto._
+&nbsp;
+# **Para testar o projeto:** 
+&nbsp;
+**1. Clone o Repositorio**
+```sh
+$ git clone https://github.com/alocs929/GoStack-conceitos-do-nodejs.git
+```
+&nbsp;
+**2. Inicie o yarn na pasta "GoStack-conceitos-do-nodejs"**
+```sh
+$ yarn
+```
+##### Este comando vai dar um stat no projeto baixando as dependências e criando a pasta node_modules.
+&nbsp;
+
+**3. Start o projeto como test**
+```sh
+$ yarn test
+```
+&nbsp;
+
+[![MeuGit](https://img.shields.io/badge/All-Projects-blue)](https://github.com/alocs929?tab=repositories)  [![Linkedin](https://img.shields.io/badge/My-Linkedin-blue)](https://www.linkedin.com/in/fbrunormartins/)
+> Bruno Martins
--- src/app.js
+++ src/app.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-
-// const { uuid } = require("uuidv4");
+const { uuid, isUuid } = require("uuidv4");
 
 const app = express();
 
@@ -10,24 +9,81 @@ app.use(cors());
 
 const repositories = [];
 
+function validateRepository(request, resposne, next){
+  const {id} = request.params;
+  
+  if(!isUuid(id)){
+    return resposne.status(400).json({ 
+      error: 'Id invalid.'
+    });
+  }
+  
+  return next();
+};
+
+
+app.use('/repositories/:id', validateRepository);
+
 app.get("/repositories", (request, response) => {
-  // TODO
+  return response.json(repositories);
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  const { title, url, techs} = request.body;
+  const repository = {
+    id: uuid(),
+    title,
+    url,
+    techs,
+    likes: 0
+  };
+
+  repositories.push(repository);
+
+  return response.json(repository);
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  const {id} = request.params;
+  const {title, url, techs,likes} = request.body;
+  
+  const repIndex = repositories.findIndex( rep => rep.id === id);
+  if(repIndex < 0){
+    return response.status(400).json({erro: 'Repository not found // ID.'});
+  }
+
+  console.log(repositories[repIndex]);
+  console.log(" ");
+  const repository = {
+    id,
+    title,
+    url,
+    techs,
+    likes: repositories[repIndex].likes,
+  };
+  console.log(repository);
+  repositories[repIndex] = repository;
+  return response.json(repository); 
+
 });
 
 app.delete("/repositories/:id", (request, response) => {
-  // TODO
+  const {id} = request.params;
+  
+  const repIndex = repositories.findIndex( rep => rep.id === id);
+  if(repIndex < 0){
+    return response.status(400).json({erro: 'Repository not found // ID.'});
+  }
+  repositories.splice(repIndex, 1);
+  
+  return response.status(204).send();
 });
 
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+  const {id} = request.params;
+  const repository = repositories.find(rep => rep.id === id);
+  repository.likes++;
+  return response.json(repository);
 });
 
 module.exports = app;
